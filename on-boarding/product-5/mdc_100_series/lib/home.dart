18cc71470717e75da8c9473edf57ccf7867cdb48
iamsamienda <samuelendale7373@gmail.com>
2024-08-07 14:35:24 +0300

    complete Layout basics codelab

--- on-boarding/product-5/mdc_100_series/lib/home.dart
+++ on-boarding/product-5/mdc_100_series/lib/home.dart
@@ -13,24 +13,116 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shrine/model/product.dart';
+import 'package:shrine/model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   // TODO: Make a collection of cards (102)
+  
+List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map(
+      (product) {
+        return Card(
+          clipBehavior: Clip.antiAlias,
+
+          // elevation: 3,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AspectRatio(
+                aspectRatio: 18 / 11,
+                child: Image.asset(
+                  product.assetName,
+                  package: product.assetPackage,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 12, 16, 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      product.name,
+                      style: theme.textTheme.titleLarge,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.titleSmall,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    ).toList();
+  }
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
-      
-      // TODO: Add app bar (102)
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+    return Scaffold(
+      appBar: AppBar(
+        // elevation: 1,
+        leading: const IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+          tooltip: 'Navigational menu',
+          onPressed: null,
+        ),
+        title: const Text('SHRINE'),
+        actions: const [
+          IconButton(
+            onPressed: null,
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+            tooltip: 'Search',
+          ),
+          IconButton(
+            onPressed: null,
+            icon: Icon(
+              Icons.tune,
+              color: Colors.black,
+            ),
+            tooltip: 'Filter',
+          )
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16),
+        childAspectRatio: 8 / 9,
+        children: _buildGridCards(context),
       ),
       resizeToAvoidBottomInset: false,
     );
   }
 }
+
+
+
